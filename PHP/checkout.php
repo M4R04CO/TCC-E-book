6d80559887a7a44d36349aed94b69f96984a31c6
M4R04C <ma4834282@gmail.com>
2023-10-27 17:47:00 -0300

    atual

---
+++ PHP/checkout.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <title>CHECKOUT</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="checkout.css">
+
+  </head>
+  <body>  
+    <header>
+        <i class='bx bx-menu'></i>
+        <img src="img/logo.png" alt="LOGO" style="width:25%">
+
+        <i><a href="usuario.php"><img src="" alt="USUÁRIO"></a></i>
+        <i><a href="carrinho.php"><img src="" alt="CARRINHO"></a></i>
+        <i><a href="salvo.php"><img src="" alt="SALVOS"></a></i>
+    </header>
+
+    <section>
+        <main>
+            <div class="DP">
+              <h2><img src="img/menu.png" alt="">Dados Pessoais e De Entrega</h2> <hr>
+              <p>Já Possui Cadastro? <a href="">Identifique-se</a></p> <hr>
+            </div>
+        </main>
+        <main>
+
+        </main>
+        <main>
+
+        </main>
+    </section>
+
+    <footer>
+      <div class="foot">
+        <img src="img/MP.png" alt="LINK PARA MERCADO PAGO">
+        <p>
+          Escolha como pagar Com o Mercado Pago, você paga com cartão, boleto ou Pix. Você também pode pagar em até 12x no boleto com o Mercado 
+          Crédito. <a href="">Como pagar com Mercado Pago.</a>
+        </p>
+      </div>
+
+      <hr>
+
+      <div class="foot">
+        <img src="img/protecao.png" alt="COMO IREMOS TE PROTEGER">
+        <p>
+        Segurança, do início ao fim. Você não gostou do que comprou? Devolva! No Mercado Livre não há nada que você não possa fazer, 
+        porque você está sempre protegido. <a href="">Como te protegemos.</a>
+        </p>
+      </div>
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
